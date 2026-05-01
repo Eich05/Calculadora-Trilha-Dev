ef7a5cadd720817e005c8325d7161f41978237db
Eich <lucas.eich@saipos.com>
2026-05-01 05:18:45 -0300

    C09 - DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,10 +1,22 @@
 class CalcController {
-    
+
     constructor(){
 
         this._displayCal = "0";
-        this._dataAtual; 
+        this._currentDate; 
+        this.initialize();
 
+    }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567"
+        dateEl.innerHTML = "01/05/2026"
+        timeEl.innerHTML = "05:14"
     }
 
     get displayCalc(){
@@ -15,12 +27,12 @@ class CalcController {
         this.displayCalc = valor;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 
 }
