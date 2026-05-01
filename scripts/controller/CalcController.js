d1123f0da233e37ff097ae827bd2ceef5389fabd
Eich <lucas.eich@saipos.com>
2026-05-01 04:37:07 -0300

    C08 - Construtor e Encapsulamento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,26 @@
 class CalcController {
     
+    constructor(){
+
+        this._displayCal = "0";
+        this._dataAtual; 
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;        
+    }
+
+    set displayCalc(valor){
+        this.displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
+
 }
